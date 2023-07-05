2e060c44e0f439eb0dd49fd24c5387a29ea3e5cc
miqueas <bagente24@gmail.com>
2023-07-05 01:27:13 -0300

    terminado

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 // Imports
-require("dotenv").config();
-const port = process.env.PORT || 4000;
-const cors = require("cors");
-const express = require("express");
-const dotenv = require("dotenv");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+require('dotenv').config();
+require('ejs');
+
+// Se conecta la Base de Datos
+const { conectarDB } = require('./database');
+
+conectarDB()
 
 const app = express();
+const port = process.env.PORT || 4500
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+// app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require('./routes/reserva.routes'));
+
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+    return res.status(404).render('404');
+})
 
 // Starting the server
 app.listen(port, () => console.log(`Server on port ${port}`));
