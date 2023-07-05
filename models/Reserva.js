cc7f64a708f38c7598c877b10ccf229e03d81b49
miqueas <bagente24@gmail.com>
2023-07-05 10:00:10 -0300

    som

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,79 +1,82 @@
 // TODO: Crear modelo de datos de Reserva
-const { DataTypes, sequelize } = require('../database');
+const { DataTypes, sequelize } = require("../database");
 
-const Reserva = sequelize.define('Reserva', {
+const Reserva = sequelize.define(
+  "Reserva",
+  {
     // Model attributes are defined here
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     codigo: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        unique: true,
-        defaultValue: new Date().getTime()
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
     },
     nombre: {
-        type: DataTypes.STRING(100),
-        allowNull: false
+      type: DataTypes.STRING(100),
+      allowNull: false,
     },
     apellido: {
-        type: DataTypes.STRING(100),
-        allowNull: false
+      type: DataTypes.STRING(100),
+      allowNull: false,
     },
     fecha_ingreso: {
-        type: DataTypes.DATE,
-        allowNull: false
+      type: DataTypes.DATE,
+      allowNull: false,
     },
     fecha_salida: {
-        type: DataTypes.DATE,
-        allowNull: false,
+      type: DataTypes.DATE,
+      allowNull: false,
     },
     habitacion: {
-        type: DataTypes.STRING(100),
-        allowNull: false
+      type: DataTypes.STRING(100),
+      allowNull: false,
     },
     cantidad_personas: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     telefono: {
-        type: DataTypes.STRING(100),
-        allowNull: false
+      type: DataTypes.STRING(100),
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING(50),
-        allowNull: false
+      type: DataTypes.STRING(50),
+      allowNull: false,
     },
     estado: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
     },
     createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
     },
     updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
     },
     deletedAt: {
-        type: DataTypes.DATE,
-        allowNull: true
-    }
-}, {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
     createdAt: true,
     updatedAt: true,
     deletedAt: true,
-    tableName: 'reservas'
-});
+    tableName: "reservas",
+  }
+);
 
 // Crear tabla si no existe ({force: true} borra y crea la tabla)
 Reserva.sync({ force: false }).then(() => {
-    console.log('Tabla de Reservas creada');
+  console.log("Tabla de Reservas creada");
 });
 
 module.exports = Reserva;
